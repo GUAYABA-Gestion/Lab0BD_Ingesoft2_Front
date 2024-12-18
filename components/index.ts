02e345963860291566670d9b568283328598cbb9
DysDan <dtobarl@unal.edu.co>
2024-12-17 22:27:29 -0500

    Deleted Hero
    pague las consecuencias de mis acciones

--- components/index.ts
+++ components/index.ts
@@ -1,5 +1,4 @@
 // Export all components to import them in simpler manner
-import Hero from './Hero'
 import CustomButton from './CustomButton'
 import Tabs from './Tabs'
 import FormPersona from './FormPersona'
@@ -8,7 +7,6 @@ import FormVivienda from './FormVivienda'
 
 
 export{
-    Hero,
     CustomButton,
     Tabs,
     FormPersona,
