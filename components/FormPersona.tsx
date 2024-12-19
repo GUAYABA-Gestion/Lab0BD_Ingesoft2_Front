096559c0eeb39aff97ca75ece51974117580e49c
Miguel Suarez <89166640+miasuarezco@users.noreply.github.com>
2024-12-19 01:04:40 -0500

    Update FormPersona.tsx

--- components/FormPersona.tsx
+++ components/FormPersona.tsx
@@ -56,8 +56,8 @@ const FormPersona = () => {
                 <div className="flex space-x-4">
                     <div className="flex-auto w-[8rem]">
                         <label className="block mb-2 text-sm font-medium text-gray-900 whitespace-nowrap dark:text-white">ID:</label>
-                        <input type="text" 
-                        id="id-input"
+                        <input type="number" 
+                        id="dni-input"
                         name="dni" 
                         className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-emerald-600 dark:focus:border-emerald-600" 
                         placeholder="##########" 
